d31b8a04fa277facdf76e4268489554c3309b03b
vilmosmisota <misotavilmos@gmail.com>
2022-03-03 21:15:12 +0000

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div>
-      <h1>Sorry, We couldn't find the data</h1>
+      <h1>Oops, something has gone wrong...</h1>
       <p>
         If you keep experiencing issues with the website, please do get in
         touch: info@glassphotobook.shop
